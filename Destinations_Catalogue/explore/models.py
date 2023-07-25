f4ce020e533dc8869867a0f266ccbbcb70625c15
GalkaKG <galina_georgieva_net@abv.bg>
2023-07-25 13:41:28 +0300

    added new models in explore app

--- Destinations_Catalogue/explore/models.py
+++ Destinations_Catalogue/explore/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Continent(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class ExploreDestination(models.Model):
+    name = models.CharField(max_length=100)
+    continent = models.ForeignKey(
+        Continent,
+        on_delete=models.CASCADE,
+        related_name='destinations'
+    )
+    # description = models.TextField()
+    # image = models.ImageField(upload_to='destination_images/')
+
+
+class Attraction(models.Model):
+    name = models.CharField(max_length=100)
+    destination = models.ForeignKey(
+        ExploreDestination,
+        on_delete=models.CASCADE,
+        related_name='attractions'
+    )
+    price = models.FloatField()
+    description = models.TextField()
+    # image = models.ImageField(upload_to='attraction_images/')
+    # Add any other fields relevant to an attraction
+
+    def __str__(self):
+        return self.name
--- Destinations_Catalogue/explore/views.py
+++ Destinations_Catalogue/explore/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from Destinations_Catalogue.destinations.models import Destination
+from Destinations_Catalogue.explore.models import Continent
 
 
 class ExploreView(ListView):
-    model = Destination
+    model = Continent
     template_name = 'explore/explore.html'
     context_object_name = 'destinations'
